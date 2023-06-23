d66563d95cd48a3bb2e95e9c3c2835e731adbf9f
Chaplik-Gabriel <gabrielchaplik@gmail.com>
2023-06-23 12:12:59 -0400

    finishing EZChatp1.py

--- EZChatP1.py
+++ EZChatP1.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemm = PorterStemmer()
@@ -9,6 +10,11 @@ def LS(word):
     return stemm.stem(word.lower())
 
 def BOW(TSent,low):
-    pass
+    TSent=[LS(w) for w in TSent]
+    bag=np.zeros(len(low),dtype=np.float32)
+    for index,w in enumerate(low):
+        if w in TSent:
+            bag[index]=1.0
+    return bag
 
 
--- training.py
+++ training.py
@@ -1,5 +1,6 @@
 import json
 from EZChatP1 import token,BOW,LS
+import numpy as np
 with open("ourjsonFile",'r') as f:
     intents = json.load(f)
 
@@ -21,3 +22,12 @@ allWords=[LS(w) for w in allWords if w not in ignore]
 allWords=sorted(set(allWords))
 tags=sorted(set(tags))
 
+xtrain=[]
+ytrain=[]
+for(patternS,tag) in xy:
+    bag=BOW(patternS,allWords)
+    xtrain.append(bag)
+    l = tags.index(tag)
+    ytrain.append(l)
+xtrain=np.array(xtrain)
+ytrain=np.array(ytrain)
