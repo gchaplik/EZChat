b6abc5d75fbd5522f283627960321c770efa47a1
Chaplik-Gabriel <gabrielchaplik@gmail.com>
2023-06-25 01:19:15 -0400

    bug fixes

--- training.py
+++ training.py
@@ -6,9 +6,8 @@ import torch.nn as nn
 from torch.utils.data import Dataset,DataLoader
 from ml_model import NeuralNetwork
 
-with open("ourjsonFile",'r') as f:
+with open("data_tags.json",'r') as f:
     intents = json.load(f)
-
 allWords=[]
 tags=[]
 xy=[]
@@ -16,54 +15,54 @@ xy=[]
 for intent in intents['intents']:
     tag = intent['tag']
     tags.append(tag)
-    for pattern in intent['patterns']:
+    for pattern in intent["patterns"]:
         w=token(pattern)
         allWords.extend(w)
         xy.append((w,tag))
-
 ignore=["?",".","/",",","+","-","(",")"]
 
 allWords=[LS(w) for w in allWords if w not in ignore]
 allWords=sorted(set(allWords))
 tags=sorted(set(tags))
+
 xtrain=[]
 ytrain=[]
 for(patternS,tag) in xy:
     bag=BOW(patternS,allWords)
+    
     xtrain.append(bag)
     l = tags.index(tag)
     ytrain.append(l)
 xtrain=np.array(xtrain)
 ytrain=np.array(ytrain)
 
-class CDS(ds):
+class CDS(Dataset):
     def __init__(self):
-        self.numSamples=len(xtrain)
+        self.n_samples=len(xtrain)
         self.xData=xtrain
         self.yData=ytrain
-    def __get__(self,index):
+    def __getitem__(self,index):
         return self.xData[index],self.yData[index]
     def __len__(self):
-        return self.numSamples
+        return self.n_samples
 
-# params
-bs=8
+#params
+batchsize=8
 
-# let the input size based off the 1st bag of words
+#let the input size based off the 1st bag of words
 hidden_layers = 8
 output_size = len(tags)
 input_size = len(xtrain[0])
 learn_rate = 0.001
 num_epochs = 1000 # 1000 iterations
 
-ds=CDS()
-tLoader = DataLoader(dataset=ds,batch_size=bs,shuffle=True,num_workers=2)
+dataset=CDS()
+tLoader = DataLoader(dataset = dataset , batch_size = batchsize , shuffle = True , num_workers = 0)
 
-# define model within training set
+#define model within training set
 user_device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
-model = NeuralNetwork(input_size, hidden_layer_size, num_classes)
-
-# loss and optimizer parameters
+model = NeuralNetwork(input_size, hidden_layers, num_epochs)
+#loss and optimizer parameters
 criteria = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr = learn_rate)
 
@@ -71,19 +70,22 @@ for epoch in range(num_epochs):
     for (words, labels) in tLoader:
         words = words.to(user_device)
         labels = labels.to(user_device)
-        
-        # fwd activation
-        outputs = model(word)
+
+#fwd activation
+        outputs = model(words)
         loss = criteria(outputs, labels)
-        
-        # optimizer and backtrack
+
+#optimizer and backtrack
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        # debug output
+
+#debug output
         if (epoch + 1) % 100 == 0:
             print(f'Epoch {epoch + 1}/{num_epochs}, loss = {loss.item()}') 
 
 print(f'Final model loss: {loss.item()}')
+       
+
+
         
